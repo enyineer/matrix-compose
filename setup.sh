8d9e6a3e62ac1eaaf45e9ee6982a0ff69b70b0ea
Nico <nico.enking@gmail.com>
2022-01-28 15:07:40 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+#sudo chmod a+x /usr/local/bin/yq
+#sudo apt install moreutils
+sudo docker network create --driver=bridge --subnet=10.10.10.0/24 --gateway=10.10.10.1 matrix_backend
+sudo docker network create --driver=bridge --subnet=10.10.11.0/24 --gateway=10.10.11.1 matrix_frontend
+# Create conf, then edit matrix/synapse/homserver.yml
+sudo docker run -it --rm -v "./matrix/synapse:/data" -e SYNAPSE_SERVER_NAME=home.xhosted.de -e SYNAPSE_REPORT_STATS=no matrixdotorg/synapse:latest generate
+# Edit database:
+# database:
+#   name: psycopg2
+#   args:
+#     user: synapse
+#     password: STRONGPASSWORD
+#     database: synapse
+#     host: postgres
+#     cp_min: 5
+#     cp_max: 10
